6d0e728224580f865a6d98f765b9b872990668d9
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 11:23:04 -0300

    commit inicial

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade minima do estoque.");
+            int quantidadeMinima = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade maxima");
+            int quantidadeMaxima = int.Parse(Console.ReadLine());
+            int Calculo = CalcularMedia(quantidadeMinima, quantidadeMaxima);
+            Console.WriteLine("A quantidade Media é de: "+Calculo);
+            Console.ReadLine();
+        }
+        public static int CalcularMedia(int minimo, int max)
+        {
+            int estoqueMedio = (minimo + max) / 2;
+            return estoqueMedio;
+        }
+    }
+}
